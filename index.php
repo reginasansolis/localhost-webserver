c15ec5cf636a75959aef0584bc67bb76336e7d0c
Regina Sansolis <regina_sansolis003@yahoo.com>
2018-05-24 21:50:05 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<body>
+<title>RHOMBUS</title>
+
+<div align="center">
+<font color="black">
+<h2>File Storage</h2>
+
+
+<?php
+include 'myconnect.php';
+$conn = OpenCon();
+echo "Predictive Model on Rookie Athletes 
+Performances using Decision Tree Analysis";
+CloseCon($conn);
+?>
+<br><br><br>
+
+<div align="center">
+<form action="one.html" method="get">
+<button name="proceed" type="submit">Proceed >>></button><br>
+</form>
+</div>
+
+</div>
+</body>
+</html>
